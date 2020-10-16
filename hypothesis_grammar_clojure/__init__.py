a351f47b1a10d9dd6426e9b08ac84fea9864b15c
sogaiu <983021772@users.noreply.github.com>
2020-10-17 08:20:28 +0900

    Provide *_items via __init__.py

--- hypothesis_grammar_clojure/__init__.py
+++ hypothesis_grammar_clojure/__init__.py
@@ -2,50 +2,6 @@
 
 __version__ = '0.0.1'
 
-# grammatical pieces
-#
-# subatomic (1)
-#   + auto_res_marker
-#
-# atomic (10)
-#   + boolean
-#   + character
-#   + comment
-#   + keyword
-#   + nil
-#   + number
-#   + regex
-#   + string
-#   + symbol
-#   + symbolic_value
-#
-# collection-like (8)
-#   + anon_func
-#   + list
-#   + map
-#   + namespaced_map
-#   + read_cond
-#   + read_cond_splicing
-#   + set
-#   + vector
-#
-# adorned form (7)
-#   + deref_form
-#   + eval_form
-#   + quote_form
-#   + syntax_quote_form
-#   + unquote_form
-#   + unquote_splicing_form
-#   + var_quote_form
-#
-# other (2)
-#   + discard_expr
-#   + tagged_literal
-#
-# compound but not standalone (2)
-#   + metadata
-#   + old_metadata
-
 # potential users
 #
 # - calva (see src/cursor-doc)
@@ -59,3 +15,52 @@ __version__ = '0.0.1'
 #     https://github.com/lread/rewrite-cljc-playground
 # - tree-sitter-clojure
 #     https://github.com/sogaiu/tree-sitter-clojure
+
+# XXX: there might be some things missing from below
+
+from hypothesis_grammar_clojure.comments import comment_items
+#
+from hypothesis_grammar_clojure.nils import nil_items
+from hypothesis_grammar_clojure.booleans import boolean_items
+from hypothesis_grammar_clojure.characters import character_items
+from hypothesis_grammar_clojure.keywords import keyword_items
+from hypothesis_grammar_clojure.numbers import number_items
+from hypothesis_grammar_clojure.strings import string_items
+from hypothesis_grammar_clojure.symbols import symbol_items
+from hypothesis_grammar_clojure.symbolic_values import symbolic_value_items
+from hypothesis_grammar_clojure.regex import regex_items
+#
+from hypothesis_grammar_clojure.atoms import atom_items
+#
+from hypothesis_grammar_clojure.lists import list_items
+from hypothesis_grammar_clojure.maps import map_items
+from hypothesis_grammar_clojure.namespaced_maps import namespaced_map_items
+from hypothesis_grammar_clojure.sets import set_items
+from hypothesis_grammar_clojure.vectors import vector_items
+#
+from hypothesis_grammar_clojure.collections import collection_items, \
+    recursive_collection_items
+#
+from hypothesis_grammar_clojure.read_conds import read_cond_items
+from hypothesis_grammar_clojure.read_cond_splicings import \
+    read_cond_splicing_items
+#
+from hypothesis_grammar_clojure.anon_funcs import anon_func_items
+#
+from hypothesis_grammar_clojure.deref_forms import deref_form_items
+from hypothesis_grammar_clojure.var_quote_forms import var_quote_form_items
+from hypothesis_grammar_clojure.eval_forms import eval_form_items
+from hypothesis_grammar_clojure.quote_forms import quote_form_items
+from hypothesis_grammar_clojure.syntax_quote_forms import \
+    syntax_quote_form_items
+from hypothesis_grammar_clojure.unquote_forms import unquote_form_items
+from hypothesis_grammar_clojure.unquote_splicing_forms import \
+    unquote_splicing_form_items
+#
+from hypothesis_grammar_clojure.tagged_literals import tagged_literal_items
+#
+from hypothesis_grammar_clojure.discard_exprs import discard_expr_items
+#
+from hypothesis_grammar_clojure.forms import form_items
+#
+from hypothesis_grammar_clojure.metadata import metadata_items
