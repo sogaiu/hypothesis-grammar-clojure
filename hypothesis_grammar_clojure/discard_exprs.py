27c6e5d2e71575eb526f677436147d94d7eab1aa
sogaiu <983021772@users.noreply.github.com>
2020-10-23 21:31:26 +0900

    Add elements parameter to discard_expr_items

--- hypothesis_grammar_clojure/discard_exprs.py
+++ hypothesis_grammar_clojure/discard_exprs.py
@@ -1,25 +1,24 @@
 from hypothesis.strategies import composite
 
+from .forms import form_items
+
 from custom.label.discard_exprs import label
 from custom.verify.discard_exprs import verify
 
 marker = '#_'
 
 def build_discard_expr_str(item):
-    form_item = item["inputs"]
+    some_item = item["inputs"]
     # XXX: space here should really be separator, but can
     #      also be empty string
-    return marker + " " + form_item["to_str"](form_item)
+    return marker + " " + some_item["to_str"](some_item)
 
 # XXX: make another key-value pair for the repeat non_form?
 @composite
-def discard_expr_items(draw):
-    # avoid circular dependency
-    from .forms import form_items
+def discard_expr_items(draw, elements=form_items()):
+    some_item = draw(elements)
     #
-    form_item = draw(form_items())
-    #
-    return {"inputs": form_item,
+    return {"inputs": some_item,
             "label": label,
             "to_str": build_discard_expr_str,
             "verify": verify,
