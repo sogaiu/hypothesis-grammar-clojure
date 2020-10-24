e2bd4047065a836547c125e6445cdc8e5b5e8e5f
sogaiu <983021772@users.noreply.github.com>
2020-10-24 16:43:43 +0900

    Fill out whitespace generator

--- hypothesis_grammar_clojure/whitespace.py
+++ hypothesis_grammar_clojure/whitespace.py
@@ -9,8 +9,29 @@ def build_whitespace_str(item):
 
 @composite
 def whitespace_items(draw):
-    # XXX: java.lang.Character isWhitespace has more...
-    allowed = ["\f", "\n", "\r", "\t", ",", " "]
+    # see java.lang.Character isWhitespace
+    allowed = [",", # clojure treats a comma as whitespace
+               # space separators excluding U+00A0, U+2007, U+202F
+               " ",
+               '\u1680', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004',
+               '\u2005', '\u2006', '\u2008', '\u2009', '\u200A', '\u205F',
+               '\u3000',
+               # line separator
+               '\u2028',
+               # paragraph separator
+               '\u2029',
+               # vertical tabulation
+               '\u000B',
+               # file separator
+               '\u001c',
+               # group separator
+               '\u001d',
+               # record separator
+               '\u001e',
+               # unit separator
+               '\u001f',
+               # other things with familiar names
+               "\f", "\n", "\r", "\t"]
     #
     n = draw(integers(min_value=1, max_value=19))
     #
